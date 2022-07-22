febfd9a58f771ffa8e63ec064460b26e781d4ab1
Christabel <israelchristabel5@gmail.com>
2022-07-22 21:07:33 +0100

    shell

--- builtins.c
+++ builtins.c
@@ -1,157 +1,180 @@
 #include "shell.h"
 
 /**
- * check_for_builtins - checks if the command is a builtin
- * @vars: variables
- * Return: pointer to the function or NULL
- */
-void (*check_for_builtins(vars_t *vars))(vars_t *vars)
+ * env - Prints the environment variables to stdout
+*/
+void env(void)
 {
-	unsigned int i;
-	builtins_t check[] = {
-		{"exit", new_exit},
-		{"env", _env},
-		{"setenv", new_setenv},
-		{"unsetenv", new_unsetenv},
-		{NULL, NULL}
-	};
+	int i;
+	char **env = __environ;
 
-	for (i = 0; check[i].f != NULL; i++)
+	for (i = 0; env[i]; i++)
 	{
-		if (_strcmpr(vars->av[0], check[i].name) == 0)
-			break;
+		write(STDOUT_FILENO, env[i], _strlen(env[i]));
+		write(STDOUT_FILENO, "\n", 1);
 	}
-	if (check[i].f != NULL)
-		check[i].f(vars);
-	return (check[i].f);
+
+	set_process_exit_code(0);
 }
 
 /**
- * new_exit - exit program
- * @vars: variables
- * Return: void
- */
-void new_exit(vars_t *vars)
-{
-	int status;
-
-	if (_strcmpr(vars->av[0], "exit") == 0 && vars->av[1] != NULL)
-	{
-		status = _atoi(vars->av[1]);
-		if (status == -1)
-		{
-			vars->status = 2;
-			print_error(vars, ": Illegal number: ");
-			_puts2(vars->av[1]);
-			_puts2("\n");
-			free(vars->commands);
-			vars->commands = NULL;
-			return;
-		}
-		vars->status = status;
-	}
-	free(vars->buffer);
-	free(vars->av);
-	free(vars->commands);
-	free_env(vars->env);
-	exit(vars->status);
-}
-
-/**
- * _env - prints the current environment
- * @vars: struct of variables
- * Return: void.
- */
-void _env(vars_t *vars)
-{
-	unsigned int i;
-
-	for (i = 0; vars->env[i]; i++)
-	{
-		_puts(vars->env[i]);
-		_puts("\n");
-	}
-	vars->status = 0;
-}
-
-/**
- * new_setenv - create a new environment variable, or edit an existing variable
- * @vars: pointer to struct of variables
+ * _setenv - Sets or adds an environment variable
+ * @name: Name for the new env variable
+ * @value: Value for the new env variable
  *
- * Return: void
+ * Return: 1 on success, -1 on error
  */
-void new_setenv(vars_t *vars)
+int _setenv(char *name, char *value)
 {
-	char **key;
-	char *var;
+	int env_index, new_var_len;
 
-	if (vars->av[1] == NULL || vars->av[2] == NULL)
-	{
-		print_error(vars, ": Incorrect number of arguments\n");
-		vars->status = 2;
-		return;
+	if (validate_env_name(name) == -1)
+		return (-1);
+
+	env_index = get_env_index(name);
+	if (env_index == -1)
+	{/* var doen't exist, SO CREATE IT */
+		int env_count = 0;
+		int old_size, new_size;
+
+		while (__environ[env_count] != NULL)
+			env_count++;
+
+		old_size = sizeof(char *) * (env_count);
+		new_size = sizeof(char *) * (env_count + 2);
+		__environ = _realloc(__environ, old_size, new_size);
+		if (__environ == NULL)
+			dispatch_error("Error while _reallocating memory for new env var");
+
+		/* The new value will be stored at index env_count */
+		env_index = env_count;
+		/* last value For the new env var needs to be NULL */
+		__environ[env_count + 1] = NULL;
 	}
-	key = find_key(vars->env, vars->av[1]);
-	if (key == NULL)
-		add_key(vars);
 	else
 	{
-		var = add_value(vars->av[1], vars->av[2]);
-		if (var == NULL)
-		{
-			print_error(vars, NULL);
-			free(vars->buffer);
-			free(vars->commands);
-			free(vars->av);
-			free_env(vars->env);
-			exit(127);
-		}
-		free(*key);
-		*key = var;
+		/* var exists, so overwrite it's value */
+		free(__environ[env_index]);
 	}
-	vars->status = 0;
+
+	new_var_len = _strlen(name) + _strlen(value) + 2;
+	/* store the env var either if it exists or it needs to be overwritten */
+	__environ[env_index] = allocate_memory(sizeof(char) * new_var_len);
+	_strcpy(__environ[env_index], name);
+	_strcat(__environ[env_index], "=");
+	_strcat(__environ[env_index], value);
+
+	set_process_exit_code(0);
+	return (1);
 }
 
 /**
- * new_unsetenv - remove an environment variable
- * @vars: pointer to a struct of variables
+ * _unsetenv - Removes an evironment variable
+ * @name: Name for the new env variable
  *
- * Return: void
+ * Return: 1 on success, -1 on error
  */
-void new_unsetenv(vars_t *vars)
+int _unsetenv(char *name)
 {
-	char **key, **newenv;
+	int env_index, i;
 
-	unsigned int i, j;
+	env_index = get_env_index(name);
+	if (env_index >= 0)
+	{/* var exists, We can unset it */
+		free(__environ[env_index]);
 
-	if (vars->av[1] == NULL)
-	{
-		print_error(vars, ": Incorrect number of arguments\n");
-		vars->status = 2;
-		return;
+		for (i = env_index; __environ[i] != NULL; i++)
+			__environ[i] = __environ[i + 1];
+
+		set_process_exit_code(0);
+		return (1);
 	}
-	key = find_key(vars->env, vars->av[1]);
-	if (key == NULL)
+
+	/* Var doesn't exist, we can print error or do nothing */
+	set_process_exit_code(0); /* Indicates that no error ocurred */
+
+	return (1);
+}
+
+/**
+ * _cd - Changes the current directory of the process
+ * @path: Path to wich change the working directory
+ *
+ * Return: 1 on success, -1 on error
+*/
+int _cd(char *path)
+{
+	char buff[1024];
+	char *oldpwd;
+	char *_path = path;
+
+	if (_strcmp(path, "-") == 0)
+		path = _getenv("OLDPWD");
+
+	if (path == NULL)
 	{
-		print_error(vars, ": No variable to unset");
-		return;
+		print_builtin_error("cd: OLDPWD not set", "");
+		return (-1);
 	}
-	for (i = 0; vars->env[i] != NULL; i++)
-		;
-	newenv = malloc(sizeof(char *) * i);
-	if (newenv == NULL)
+	/* Needed to avoid reading on freed memory */
+	path = duplicate_string(path);
+	/* store this dir in case of update */
+	oldpwd = getcwd(buff, 1024);
+	if (oldpwd == NULL)
 	{
-		print_error(vars, NULL);
-		vars->status = 127;
-		new_exit(vars);
+		free(path);
+		print_builtin_error("cd: couldn't get current dir", "");
+		return (-1);
 	}
-	for (i = 0; vars->env[i] != *key; i++)
-		newenv[i] = vars->env[i];
-	for (j = i + 1; vars->env[j] != NULL; j++, i++)
-		newenv[i] = vars->env[j];
-	newenv[i] = NULL;
-	free(*key);
-	free(vars->env);
-	vars->env = newenv;
-	vars->status = 0;
+	/* Try to change the current dir */
+	if (chdir(path) == -1)
+	{
+		free(path);
+		print_builtin_error("cd: can't change cd to ", _path);
+		set_process_exit_code(1);
+		return (-1);
+	}
+	/* Update env variables */
+	_setenv("OLDPWD", oldpwd);
+	_setenv("PWD", path);
+	free(path);
+	set_process_exit_code(0);
+	return (1);
+}
+
+/**
+ * _alias - Sets an alias command
+ * @commands: List of commands
+ *
+ * Return: -1 on error, 0 otherwise
+*/
+int _alias(char **commands)
+{
+	int status = 0;
+	list_t *curr;
+	list_t *out_head = NULL;
+	list_t **alias_addrs = get_alias_head();
+
+	/* the alias args starts from position 1 */
+	if (commands[1] == NULL)
+	{ /* This means to list all the aliases */
+		for (curr = *alias_addrs; curr != NULL; curr = curr->next)
+		{
+			_puts(curr->str);
+			_puts("\n");
+		}
+		set_process_exit_code(0);
+		return (1);
+	}
+	/* List aliases and sets the aliases that have the form name=value */
+	status = handle_alias_args(commands, &out_head);
+	/* print listed alias */
+	for (curr = out_head; curr != NULL; curr = curr->next)
+	{
+		_puts(curr->str);
+		_puts("\n");
+	}
+	/* free list */
+	free_list(out_head);
+	return (status);
 }
